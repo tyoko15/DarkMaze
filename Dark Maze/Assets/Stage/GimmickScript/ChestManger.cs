816bb0b6f2ed034f23608df2c48fe098be30d248
ktc23a31a0021 <ktc23a31a0021@edu.kyoto-tech.ac.jp>
2025-07-29 22:29:24 +0900

    チェストの演出の追加

--- Dark Maze/Assets/Stage/GimmickScript/ChestManger.cs
+++ Dark Maze/Assets/Stage/GimmickScript/ChestManger.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ChestManger : MonoBehaviour
 {
     [SerializeField] GameObject chestObject;
+    GameObject chestTopObject;
     [SerializeField] GameObject player;
     [SerializeField] int itemNum;
     bool openFlag;
@@ -11,7 +12,8 @@ public class ChestManger : MonoBehaviour
     [SerializeField] bool hideFlag;
     void Start()
     {
-        if(hideFlag) chestObject.SetActive(false);
+        if (hideFlag) chestObject.SetActive(false);
+        if (chestObject.transform.childCount > 1) chestTopObject = chestObject.transform.GetChild(0).gameObject;
     }
 
     void Update()
@@ -30,6 +32,15 @@ public class ChestManger : MonoBehaviour
             else if(openedTimer < openedTime)
             {
                 openedTimer += Time.deltaTime;
+                if (chestTopObject != null)
+                {
+                    if (openedTimer > openedTime * 0.5f) chestTopObject.transform.eulerAngles = new Vector3(290f, 180f, 0f);
+                    else if (openedTimer < openedTime * 0.5f)
+                    {
+                        float x = Mathf.Lerp(360f, 290f, openedTimer / (openedTime * 0.5f));
+                        chestTopObject.transform.eulerAngles = new Vector3(x, 180f, 0f);
+                    }
+                }
             }
         }
     }
