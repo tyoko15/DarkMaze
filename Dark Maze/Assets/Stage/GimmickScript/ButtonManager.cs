d1b360fb40acc8bf9d5785a1e01ba0f49a7b08a8
6F\ktc <>
2025-07-30 12:34:03 +0900

    Buttonの演出追加&ParticleSystemでエフェクト編集中&Chestの演出追加中

--- Dark Maze/Assets/Stage/GimmickScript/ButtonManager.cs
+++ Dark Maze/Assets/Stage/GimmickScript/ButtonManager.cs
@@ -1,5 +1,7 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem.Composites;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
@@ -9,10 +11,50 @@ public class ButtonManager : MonoBehaviour
     [SerializeField] bool senceFlag;
     [SerializeField] public bool completeFlag;
     int somethingNum;
+
+    bool moderingFlag;
+    GameObject buttonObject;
+    GameObject buttonLight;
+    // Crystal‚ÌOn Off 0.On 1.Off
+    [SerializeField] Color[] buttonCrystalColors;
+    // 0.Stand 1.Crystal
+    Material[] buttonMaterials;
+
     private void Start()
     {
         if(hideFlag) gameObject.SetActive(false);
+        buttonObject = gameObject;
+        if (buttonObject.GetComponent<MeshRenderer>())
+        {             
+            int materialCount = buttonObject.GetComponent<MeshRenderer>().materials.Length;
+            buttonMaterials = new Material[materialCount];
+            for (int i = 0; i < materialCount; i++) buttonMaterials[i] = buttonObject.GetComponent<MeshRenderer>().materials[i];
+            if (buttonObject.transform.childCount != 0)
+            {
+                buttonLight = buttonObject.transform.GetChild(0).gameObject;
+                buttonLight.SetActive(false);
+            }
+            moderingFlag = true;
+        }
     }
+
+    void Update()
+    {
+        if (moderingFlag)
+        {
+            if (buttonFlag) 
+            {
+                buttonMaterials[1].color = buttonCrystalColors[0];
+                buttonLight.SetActive(true);
+            }            
+            else            
+            {
+                buttonMaterials[1].color = buttonCrystalColors[1];
+                buttonLight.SetActive(false);
+            }            
+        }
+    }
+
     private void OnTriggerEnter(Collider collision)
     {
         if (!senceFlag)
