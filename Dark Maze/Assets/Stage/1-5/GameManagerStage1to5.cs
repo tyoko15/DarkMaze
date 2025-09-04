8aef70bd327b2d3015eebf3796c9e7f1c5fdcae2
ibuki-27 <KTC25A31A0050@edu.kyoto-tech.ac.jp>
2025-09-04 16:13:31 +0900

    ステージ1-5&ステージ2-5の形だけ追加

---
+++ Dark Maze/Assets/Stage/1-5/GameManagerStage1to5.cs
@@ -0,0 +1,39 @@
+public class GameManagerStage1to5 : GeneralStageManager
+{
+    void Start()
+    {
+        StartData();
+    }
+
+    void Update()
+    {
+        switch (status)
+        {
+            case GameStatus.start:
+                playerController.status = 0;
+                StartAnime();
+                break;
+            case GameStatus.play:
+                Goal();
+                if (menuFlag) status = GameStatus.menu;
+                playerController.status = 1;
+                break;
+            case GameStatus.stop:
+                Goal();
+                playerController.status = 2;
+                break;
+            case GameStatus.menu:
+                MenuUIControl();
+                if (!menuFlag) status = GameStatus.play;
+                playerController.status = 3;
+                break;
+            case GameStatus.over:
+                playerController.status = 4;
+                break;
+            case GameStatus.clear:
+                playerController.status = 5;
+                EndAnime();
+                break;
+        }
+    }
+}
